0200267b3b5795bd990df6b864a223afd782e4bd
alaric.ouyang <alaric.ouyang@ringcentral.com>
2023-10-08 19:34:27 +0800

    调试问题

--- spanai-api/src/module/summary/summary.service.ts
+++ spanai-api/src/module/summary/summary.service.ts
@@ -22,8 +22,9 @@ export class SummaryService {
   async getMessageWithSummary(chatId: string, messages: Message[], model: any) {
     const summary = await this.summaryModel.findOne({ chat: chatId });
     if (!summary) {
-      const finalMessages = messages.length > 7 ? messages.slice(messages.length - 7) : messages;
-      return { messages: finalMessages, summary: null };
+      return { messages, summary: null };
+      // const finalMessages = messages.length > 7 ? messages.slice(messages.length - 7) : messages;
+      // return { messages: finalMessages, summary: null };
     }
     const totalMessageCount = messages.length;
 
